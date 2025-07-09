e0c29b0315a09a49c64e94c390f32b0bf369f05d
Amha Shiferaw <amhashiferaw@gmail.com>
2025-07-09 01:44:26 +0000

    added variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+    description = "Project name for tagging resources"
+    default     = "invision-connect"
+}
+
+variable "aws_region" {
+    description = "AWS region to deploy resources"
+    default     = "us-east-1"
+}
